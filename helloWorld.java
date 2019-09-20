75ac28944d1136175aa5aae787d34a7d9588ea81
dunderwood <drew.underwood@nisc.coop>
2019-09-20 18:53:41 -0500

    My first commit

--- helloWorld.java
+++ helloWorld.java
@@ -1,3 +1,5 @@
-public static void main(String args[]) {
-  System.out.println("Hello World");
+public class helloWorld {
+  public static void main(String args[]) {
+    System.out.println("Hello World");
+  }  	
 }
